0a995604520693e5add99bef710a21d9542780ba
Maddie Galvin <mgalvin4@nd.edu>
2020-10-23 10:31:42 -0400

    try again

--- Exercise8Answers.R
+++ Exercise8Answers.R
@@ -1,41 +1,17 @@
-# Exercise 8
+# Galvin - DiDonato Exercise 8 
 
 setwd("/Users/Maddie Galvin/Desktop/Biocomputing2020_Tutorial10/")
-library(tibble)
-library(dplyr)
-# 1. plot sports scores
+
+### 1. plot sports scores
+
 df <- read.table(file="UWvMSU_1-22-13.txt",header=TRUE,sep="", stringsAsFactors=FALSE)
 #sportsMatrix <- as.matrix(df)
-#UWscore = sum(df$score[df$team=="UW"])
-#MSUscore = sum(df$score[df$team=="MSU"])
+
 time = df$time
 score = df$score
 
-UWscore = 0
-MSUscore = 0
-for (i in 1:length(df$time)){
-  if (df$team[i]=="UW"){
-    UWscore = UWscore + df$score[i]
-  }
-  else {
-    MSUscore = MSUscore + df$score[i]
-  }
-}
-
-UWscore
-MSUscore
-
-for (i in 1:length(df$time)){
-  df["UWscore"] <- UWscore[i]
-  df["MSUscore"] <- MSUscore[i]
-}
-
-df
-
-plot(time,UWscore,type='l', col="blue")
-lines(time, MSUscore, type='l', col="red")
-
-## new solution
+# we got so close to solving this question, but we couldn't make
+# the cumulative score work right. Here's our best solution:
 
 UWpt=0
 MSUpt=0
@@ -51,46 +27,26 @@ for (i in 1:length(df$time)){
   }
 }
 
-##beth solution
+plot(time,UWscore,type='l', col="blue")
+lines(time, MSUscore, type='l', col="red")
 
-
+# here's a second possible solution:
 UWscore = 0
 MSUscore = 0
-
 for (i in 1:length(df$time)){
-  if (df$team[i]=="MSU"){
-    if (df$time[i]==4.833333){
-      MSUscore=2
-    } else{
-      df[is.na(MSUscore)]=0
-      df <- df%>%
-        mutate(MSUscore=score+lag(MSUscore))
-    }} else {
-      if (df$time[i]==3.966667){
-        UWscore=3
-      } else{
-        df[is.na(UWscore)]=0
-        df <- df%>%
-          mutate(UWscore=score+lag(UWscore))
-      }
-    }
-}
-
-for (i in 1:length(df$time)){
-  if (df$team[i]=="MSU"){
-    df[is.na(MSUscore)]=0
-    df <- df%>%
-      mutate(MSUscore=score+lag(MSUscore))
+  if (df$team[i]=="UW"){
+    UWscore = UWscore + df$score[i]
   }
   else {
-    df[is.na(UWscore)]=0
-    df <- df%>%
-      mutate(UWscore=score+lag(UWscore))
+    MSUscore = MSUscore + df$score[i]
   }
 }
 
+plot(time,UWscore,type='l', col="blue")
+lines(time, MSUscore, type='l', col="red")
 
-# 2. guess my number
+
+### 2. guess my number
 guessMyNumber <- function(answer, guess){
   answer <- sample(1:100, 1, replace=TRUE)
   guessCount = 0
